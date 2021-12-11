015ab48a209875500e869d3a06deabe0493c90c5
gusrb8925 <gusrb8925@naver.com>
2021-12-11 15:36:41 +0900

    init

---
+++ src/main/kotlin/shop/fevertime/kotlin/ChallengeApplication.kt
@@ -0,0 +1,11 @@
+package shop.fevertime.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/shop/fevertime/kotlin/ChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package shop.fevertime.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
