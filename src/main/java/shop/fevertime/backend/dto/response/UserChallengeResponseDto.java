aec7e30763d0f8c6e856405b30274ab222a8dfff
suubinkim <0vbbbv0@gmail.com>
2021-12-20 12:35:10 +0900

    fix : 유저가 생성한 챌린지 리스트 dto 수정

--- src/main/java/shop/fevertime/backend/dto/response/UserChallengeResponseDto.java
+++ src/main/java/shop/fevertime/backend/dto/response/UserChallengeResponseDto.java
@@ -2,7 +2,9 @@ package shop.fevertime.backend.dto.response;
 
 import lombok.Getter;
 import lombok.Setter;
+import shop.fevertime.backend.domain.Category;
 import shop.fevertime.backend.domain.Challenge;
+import shop.fevertime.backend.domain.LocationType;
 
 import java.time.format.DateTimeFormatter;
 
@@ -12,15 +14,28 @@ import java.time.format.DateTimeFormatter;
 @Getter
 @Setter
 public class UserChallengeResponseDto {
+    private Long challengeId;
     private String title;
+    private String description;
     private String challengeStartDate;
     private String challengeEndDate;
-    private Long challengeId;
+    private int limitPerson;
+    private LocationType locationType;
+    private String address;
+    private String imgUrl;
+    private String category;
 
     public UserChallengeResponseDto(Challenge challenge) {
+        this.challengeId = challenge.getId();
         this.title = challenge.getTitle();
+        this.description = challenge.getDescription();
         this.challengeStartDate = challenge.getStartDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.challengeEndDate = challenge.getEndDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        this.challengeId = challenge.getId();
+        this.limitPerson = challenge.getLimitPerson();
+        this.locationType = challenge.getLocationType();
+        this.address = challenge.getAddress();
+        this.imgUrl = challenge.getImgUrl();
+        this.category = challenge.getCategory().getName();
+
     }
 }
