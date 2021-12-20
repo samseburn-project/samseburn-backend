1d6c403666c17c7bbb47deace2b61a5ed77951e0
suubinkim <0vbbbv0@gmail.com>
2021-12-20 14:16:35 +0900

    feat : 챌린지 인증 수정 api 추가

---
+++ src/main/java/shop/fevertime/backend/dto/request/CertiUpdateRequestDto.java
@@ -0,0 +1,10 @@
+package shop.fevertime.backend.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CertiUpdateRequestDto {
+    private String contents;
+}
