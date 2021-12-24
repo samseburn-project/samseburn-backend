3f4c5a4ddbe231b82256e9bf26374bb182378e07
jackson <steady8@kakao.com>
2021-12-24 15:11:25 +0900

    feat: Challemge Mission Scheduler ( #34 )

--- src/main/java/shop/fevertime/backend/util/Scheduler.java
+++ src/main/java/shop/fevertime/backend/util/Scheduler.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import shop.fevertime.backend.domain.Challenge;
+import shop.fevertime.backend.domain.ChallengeHistory;
 import shop.fevertime.backend.domain.ChallengeProgress;
 import shop.fevertime.backend.exception.ApiRequestException;
+import shop.fevertime.backend.repository.CertificationRepository;
+import shop.fevertime.backend.repository.ChallengeHistoryRepository;
 import shop.fevertime.backend.repository.ChallengeRepository;
 
 import javax.transaction.Transactional;
@@ -17,6 +20,8 @@ import java.time.chrono.ChronoLocalDate;
 public class Scheduler {
 
     private final ChallengeRepository challengeRepository;
+    private final ChallengeHistoryRepository challengeHistoryRepository;
+    private final CertificationRepository certificationRepository;
 
     @Scheduled(cron = "0 0 0 * * *")
     @Transactional
@@ -24,6 +29,19 @@ public class Scheduler {
         for (Challenge challenge : challengeRepository.findAllByChallengeProgress(ChallengeProgress.INPROGRESS)) {
             if (LocalDate.now().isAfter(challenge.getEndDate().toLocalDate())) {
                 challenge.stopChallnegeProgress();
+            }
+        }
+    }
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    public void updateChallengeStatus() throws ApiRequestException {
+        // 챌린지 mission_date가 됐을 때, 인증 횟수가 3회 넘지 않으면
+        for (ChallengeHistory challengeHistory : challengeHistoryRepository.findAllByMissionDateBefore(LocalDate.now())) {
+            int certiCount = certificationRepository.findAllByChallenge(challengeHistory.getChallenge()).size();
+            if (certiCount < 3) {
+                challengeHistory.fail(); // JOIN -> RETRY
+            } else {
+                challengeHistory.cancel(); // JOIN -> FAIL
             }
         }
     }
